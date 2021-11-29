2b7d3855a842da1cfa7251e16ad8e6ff6737dd03
SamuelLuisGomes <samuelluisgomestech@gmail.com>
2021-11-29 20:31:52 +0000

    projeto-finalizado

--- app.py
+++ app.py
@@ -1,12 +1,29 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
 
+mangas = [
+
+]
 
 @app.route('/')
-def hello():
-    return render_template('index.html')
+def index():
+    return render_template('index.html', lista=mangas)
+
+@app.route('/cadastro')
+def cadastro():
+    return render_template('cadastro.html')
 
 
+@app.route('/salvar', methods=['POST'])
+def salvar():
+
+    nome = request.form['nome']
+    autor = request.form['autor']
+
+    manga  = { 'Nome' : f' { nome } ' , 'Autor' : f' { autor }'}
+    mangas.append(manga)
+
+    return redirect('https://5000-yellow-warbler-yejaefyk.ws-us17.gitpod.io/', code=302)
 
 app.run(debug=True)
